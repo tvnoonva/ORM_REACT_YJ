c8f73e75f82f9c6178e0be3efc8aaee303ee0163
yujin <am3ca723@gmail.com>
2024-02-08 17:10:51 +0900

    0208 calculator

--- DAY4/script.js
+++ DAY4/script.js
@@ -1,13 +1,16 @@
 let input = document.getElementById('input');
 
 function appendNumber(number) {
+    console.log("현재value: ", input.value);
     input.value += number;
 }
 
 function appendOperator(operator) {
+    console.log("현재value: ", input.value);
     //마지막 글자가 operator일경우 마지막글자를 새로 input한 값으로 덮어씌우기
-    console.log(getLastNumber());
-    if (getLastNumber()) {
+    let getNum = getLastNumber();
+    console.log(getNum);
+    if (getNum) {
         input.value += operator;
     } else {
         input.value = input.value.slice(0, -1) + operator;
@@ -18,32 +21,34 @@ function appendOperator(operator) {
 function getLastNumber() {
     //string의 마지막 값이 연산자일 경우 false를 return
     if (input.value.length == 0) return false;
-    let i=input.value.length -1;
+    let i = input.value.length - 1;
     let lastChar = input.value.charAt(i);
-    let getNum=0;
+    let getNum = 0;
     console.log("함수에서 검사한 값: ", lastChar);
 
     if (lastChar == '/' || lastChar == '*' || lastChar == '-' || lastChar == '+')
         return false;
     else {
-        while(i>=0){ //뒤에서부터 루프 첫 연선자의 index값 추출
+        while (i >= 0) { //뒤에서부터 루프 첫 연선자의 index값 추출
             let num = input.value[i];
-            if(num == '/' || num == '*' || num == '-' || num == '+') break;
+            if (num == '/' || num == '*' || num == '-' || num == '+') break;
             i--;
         }
-        getNum=input.value.slice(i+1,input.value.length);
+        getNum = input.value.slice(i + 1, input.value.length);
 
         return getNum;
     }
 
 }
 
+// '%' 연산함수
 function calculatePercent() {
-    //마지막으로 입력된 숫자를 /100
-    //string의 뒤에서부터 순회하며 숫자값 추출하는 로직 필요
-    let getNum=getLastNumber();
-    if(getNum){
-        
+    console.log("현재value: ", input.value);
+    let getNum = getLastNumber();
+    console.log(getNum);
+    if (getNum) {
+        input.value = input.value.slice(0, input.value.length - getNum.length);
+        input.value += getNum / 100;
     }
 }
 
@@ -57,6 +62,7 @@ function calculate() {
     try {
         input.value = eval(input.value);
     } catch (error) {
+        console.log(error);
         input.value = 'Error';
     }
 }
