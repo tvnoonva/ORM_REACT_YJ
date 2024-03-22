5f42e620a19d1bbde6ee30edcc00ce77953be303
yujin <am3ca723@gmail.com>
2024-03-22 16:59:30 +0900

    0322 실습 tailwind

--- Tailwind/nextjs-sample-app/app/blogs/page.tsx
+++ Tailwind/nextjs-sample-app/app/blogs/page.tsx
@@ -1,8 +1,9 @@
+import React, { useEffect, useState } from "react";
 const posts = [
   {
     id: 1,
     title: "Boost your conversion rate",
-    href: "#",
+    href: "/blogs/1",
     description:
       "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
     date: "Mar 16, 2020",
@@ -16,10 +17,33 @@ const posts = [
         "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
   },
+  {
+    id: 2,
+    title: "It's my blog",
+    href: "/blogs/2",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: { title: "Blog", href: "#" },
+    author: {
+      name: "Michael Foster",
+      role: "Co-Founder / CTO",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
   // More posts...
 ];
 
 export default function BlogList() {
+  // const [posts, setPosts] = useState([]);
+
+  // useEffect(()=>{
+
+  // },[]);
+
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
